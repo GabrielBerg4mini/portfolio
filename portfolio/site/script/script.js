76b8e582ba8524373769fd8953599a5dcc594272
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-01-14 11:46:11 -0300

    commit8

--- portfolio/site/script/script.js
+++ portfolio/site/script/script.js
@@ -1,9 +1,2 @@
-$('.nav a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
-});
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+console.log(menuLinks)
