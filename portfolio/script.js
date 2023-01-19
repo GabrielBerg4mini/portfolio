7580125b21674a576c91cc5e0595f37a5c6769f8
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-01-19 17:16:59 -0300

    commit18

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+const buttonJs = document.getElementById("btn-img-js");
+const modalJs = document.getElementById("btn-modal");
+const buttonClose = document.getElementById("fechar-modal");
+
+buttonJs.onclick = function() {
+    modalJs.showModal()
+};
+
+buttonClose.onclick = function() {
+    
+}
